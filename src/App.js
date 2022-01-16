82d3cbadfbb6224e1b9fe22e7568f925e9845fab
Tina <nguyennga.hcm@gmail.com>
2022-01-16 08:24:34 +0700

    add nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import Button from "@mui/material/Button";
-
+import Page1 from "./Pages/Page1";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { green, purple } from "@mui/material/colors";
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#07264c",
+      },
+      secondary: {
+        main: green[500],
+      },
+    },
+  });
   return (
-    <div className="App">
-      <Button variant="contained">Hello World</Button>
+    <div>
+      <ThemeProvider theme={theme}>
+        <Page1 />
+      </ThemeProvider>
     </div>
   );
 }
