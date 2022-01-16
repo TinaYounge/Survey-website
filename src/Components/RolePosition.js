14fe48063095892e32d5da390868fd61633f2875
Tina <nguyennga.hcm@gmail.com>
2022-01-16 17:04:32 +0700

    add page2 after fix

--- src/Components/RolePosition.js
+++ src/Components/RolePosition.js
@@ -1,27 +1,27 @@
 import * as React from "react";
-// import Button from "@mui/material/Button";
+import Button from "@mui/material/Button";
 import { Box } from "@mui/system";
 import Checkbox from "@material-ui/core/Checkbox";
 import CircleCheckedFilled from "@material-ui/icons/CheckCircle";
 import CircleUnchecked from "@material-ui/icons/RadioButtonUnchecked";
 
-const { AppBar, Button, makeStyles, Toolbar, Typography } = MaterialUI;
+// const { AppBar, Button, makeStyles, Toolbar, Typography } = MaterialUI;
 
-const useStyles = makeStyles({
-  flexGrow: {
-    flex: "1",
-  },
-  button: {
-    backgroundColor: "#3c52b2",
-    color: "#fff",
-    "&:hover": {
-      backgroundColor: "#fff",
-      color: "#3c52b2",
-    },
-  },
-});
+// const useStyles = makeStyles({
+//   flexGrow: {
+//     flex: "1",
+//   },
+//   button: {
+//     backgroundColor: "#3c52b2",
+//     color: "#fff",
+//     "&:hover": {
+//       backgroundColor: "#fff",
+//       color: "#3c52b2",
+//     },
+//   },
+// });
 export default function RolePosition() {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
     <Box>
